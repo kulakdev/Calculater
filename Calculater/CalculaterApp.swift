f174223584b0859993ff778340bec9463f917bc8
jke <constantinekulak@proton.me>
2023-06-27 14:19:20 +0300

    Initial Commit

---
+++ Calculater/CalculaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculaterApp.swift
+//  Calculater
+//
+//  Created by Constantine Kulak on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
